75c2312efc2929c48c3d33039d871048fb6564ca
Shayaroshi <tung06948@gmail.com>
2025-09-02 23:57:41 +0700

    Add if

--- projecttest1/P1App.js
+++ projecttest1/P1App.js
@@ -1,4 +1,11 @@
 alert("Website for random lottery number generation")
-var usernumber = prompt("Enter your number (1-99)")
+let usernumber = prompt("Enter your number (1-999)")
+let lotterynumber = Math.floor(Math.random() * 1000)
+if(usernumber == lotterynumber){
+    document.getElementById("alert").innerHTML = "ยินดีด้วย!! คุณถูกรางวัลเลขท้าย 3 ตัว"
+} 
+else{
+    document.getElementById("alert").innerHTML = "เสียใจด้วย คุณไม่ถูกรางวัลเลขท้าย 3 ตัว"
+}
 document.getElementById("result").innerHTML = "User = " + usernumber
-document.getElementById("random").innerHTML = "Lottery = " + Math.floor(Math.random() * 100)
+document.getElementById("random").innerHTML = "Lottery = " + lotterynumber
