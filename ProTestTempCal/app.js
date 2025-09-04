e5d5fdeba52ea81b9eca429d3c52be2e52a2a208
Shayaroshi <tung06948@gmail.com>
2025-09-04 15:41:23 +0700

    update with function

--- ProTestTempCal/app.js
+++ ProTestTempCal/app.js
@@ -1,14 +1,26 @@
-function FtoC(){
-    let choice = prompt("Enter choice : \n1. F to C \n2. C to F")
-    if(choice == 1){
-        let ftemp = prompt("Enter temperature in F : ")
-        let ctemp = (ftemp - 32) * 5/9
-        document.getElementById("result").innerHTML = "<p>" + "Temperature from F to C : " + ctemp.toFixed(2) + " C" + "</p>"
-    }
-    else if(choice == 2){
-        let ctemp = prompt("Enter temperature in C : ")
-        let ftemp = (ctemp * 9/5) + 32
-        document.getElementById("result").innerHTML = "<p>" + "Temperature from C to F : " + ftemp.toFixed(2) + " F" + "</p>"
-    }
+function FtoC(ftemp)
+{
+    let ctemp = (ftemp - 32) * 5/9
+    return "Temperature from F to C : " + ctemp.toFixed(2) + " C"
 }
-FtoC()
+function CtoF(ctemp)
+{
+    let ftemp = (ctemp * 9/5) + 32
+    return "Temperature from C to F : " + ftemp.toFixed(2) + " F"
+}
+function display(ElementId, value)
+{
+    document.getElementById(ElementId).innerHTML = "<b>" + value + "</b>"
+}
+
+var choice = prompt("Enter choice : \n1. F to C \n2. C to F")
+if(choice == 1){
+    let ftemp = prompt("Enter temperature in F : ")
+    alert(FtoC(ftemp))
+    display("result", FtoC(ftemp))
+}
+else if(choice == 2){
+    let ctemp = prompt("Enter temperature in C : ")
+    alert(CtoF(ctemp)) 
+    display("result", CtoF(ctemp))
+}
