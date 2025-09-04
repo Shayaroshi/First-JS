c45e8f6b720798fb3afd6f2d45574801ab02927d
Shayaroshi <tung06948@gmail.com>
2025-09-04 20:29:25 +0700

    update with event

--- ProTestTempCal/app.js
+++ ProTestTempCal/app.js
@@ -1,26 +1,22 @@
 function FtoC(ftemp)
 {
     let ctemp = (ftemp - 32) * 5/9
-    return "Temperature from F to C : " + ctemp.toFixed(2) + " C"
+    return ctemp.toFixed(2) + " °C"
 }
 function CtoF(ctemp)
 {
     let ftemp = (ctemp * 9/5) + 32
-    return "Temperature from C to F : " + ftemp.toFixed(2) + " F"
+    return ftemp.toFixed(2) + " °F"
 }
-function display(ElementId, value)
+// function display(ElementId, value)
+// {
+//     document.getElementById(ElementId).innerHTML = "<b>" + value + "</b>"
+// }
+function toCProgram(value)
 {
-    document.getElementById(ElementId).innerHTML = "<b>" + value + "</b>"
+    alert(FtoC(value))
 }
-
-var choice = prompt("Enter choice : \n1. F to C \n2. C to F")
-if(choice == 1){
-    let ftemp = prompt("Enter temperature in F : ")
-    alert(FtoC(ftemp))
-    display("result", FtoC(ftemp))
-}
-else if(choice == 2){
-    let ctemp = prompt("Enter temperature in C : ")
-    alert(CtoF(ctemp)) 
-    display("result", CtoF(ctemp))
+function toFProgram(value)
+{
+    alert(CtoF(value))
 }
